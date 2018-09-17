02fd1241a5b3b5f6a482678b51c0881b5d4553c4
nimp0 <cloud_nimpo@ukr.net>
2018-09-17 18:31:12 +0300

    added jumping while moving along the x axis

--- App/Apl.js
+++ App/Apl.js
@@ -1,79 +1,90 @@
-var canvas = document.querySelector("canvas");
+var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var xPos = 0;
-var xSpeed = 20;
-var yPos = 595;
-var ySpeed = 300;
-var jump = false;
-var right = false;
-var left = false;
-var fall = 5
+var player = {
+    xPos: 0,
+    yPos: 0,
+    xVelocity: 0,
+    yVelocity: 0,
+    height: 70,
+    width: 60,
+    jump: true
+};
 
-RenderPicture();
-//Falling();
+var controller = {
+    left: false,
+    right: false,
+    up: false,
 
-addEventListener("keydown", (e) => {
-    
-    if (e.key == "ArrowRight") {
-        right = true;
-        xPos += xSpeed;
-    }
-    if (e.key == "ArrowLeft") {
-        left = true;
-        xPos -= xSpeed;
-    }
-    if (jump == false && e.key == " ") {
-        jump = true;
-        yPos -= ySpeed;
-        Falling();
-    }
-    AnimatePicture();
-});
+    keyListener: function (event) {
+        var keyState = (event.type == "keydown") ? true : false;
 
-/*addEventListener("keyup", (e) => {
-    if (e.key == "ArrowRight") {
-        right = false;
-    }
-    if (e.key == "ArrowLeft") {
-        left = false;
-    }
-    AnimatePicture();
-})*/
-
-function Falling() {
-    requestAnimationFrame(Falling);
-    RenderPicture();
-    if (jump == true) {
-        if (yPos + hero.height / 10 >= canvas.height) {
-            yPos = canvas.height - hero.height / 10;
-            jump = false;
-        }
-        else {
-            yPos += fall;
+        switch (event.key) {
+            case "ArrowRight":
+                controller.right = keyState;
+                break;
+            case "ArrowLeft":
+                controller.left = keyState;
+                break;
+            case " ":
+                controller.up = keyState;
+                break;
         }
     }
 }
 
-function AnimatePicture() {
-    requestAnimationFrame(AnimatePicture);
-    RenderPicture();
+function MovementLogic() {
+
+    if (controller.left) {
+        player.xVelocity -= 2;
+    }
+    if (controller.right) {
+        player.xVelocity += 2;
+    }
+    if (controller.up && player.jump == false) {
+        player.yVelocity -= 50;
+        player.jump = true;
+    }
+
+    if (player.yPos + player.height >= canvas.height) {
+        player.yPos = canvas.height - player.height + 10;
+        player.jump = false;
+    }
+    else {
+        player.yPos += 20;
+    }
+
+    if (player.xPos + player.width >= canvas.width) {
+        player.xPos = canvas.width - player.width;
+    }
+
+    if (player.xPos - player.width <= 0) {
+        player.xPos = 0 + player.width;
+    }
+
+    player.xPos += player.xVelocity;
+    player.yPos += player.yVelocity;
+    player.xVelocity *= 0.9;
+    player.yVelocity *= 0.9;
+
+    ImportImg();
+    requestAnimationFrame(MovementLogic);
 }
 
-function RenderPicture() {
-
-    hero = new Image();
+function ImportImg() {
+    var hero = new Image();
     hero.src = "pictures/hero.png";
     hero.onload = function () {
         context.clearRect(0, 0, innerWidth, innerHeight);
-        context.beginPath();
-        context.drawImage(hero, xPos, yPos, hero.width / 10, hero.height / 10);
-        context.stroke();
+        context.drawImage(hero, player.xPos, player.yPos, player.width, player.height);
     }
 }
 
+addEventListener("keydown", controller.keyListener);
+addEventListener("keyup", controller.keyListener);
+requestAnimationFrame(MovementLogic);
 
 
 
@@ -83,7 +94,12 @@ function RenderPicture() {
 
 
 
-    
+
+
+
+
+
+
 
 
 
