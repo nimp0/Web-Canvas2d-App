273007015a8940e8446caffa41eeff4f75f10c21
nimp0 <cloud_nimpo@ukr.net>
2018-09-21 01:01:14 +0300

    edited code

--- Scripts/Apl.js
+++ Scripts/Apl.js
@@ -13,73 +13,6 @@ function LoadImg(url) {
     });
 }
 
-/*function Sprite(xPos, yPos, xIndex, yIndex, cols, rows, spriteWidth, spriteHeight) {
-
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.xIndex = xIndex;
-        this.yIndex = yIndex;
-        this.cols = cols;
-        this.rows = rows;
-        this.spriteWidth = spriteWidth;
-        this.spriteHeight = spriteHeight;
-
-    this.sheet = new Image();
-    this.sheet.src = "Pictures/trump.png";
-
-    this.sheet.onload = function () {
-
-        Animation();
-    }
-
-    function Animation() {
-        this.frames = 0;
-        requestAnimationFrame(Animation);
-
-        if ((++this.frame) % 4 > 0) return;
-        //context.clearRect(0, 0, spriteWidth, spriteHeight);
-        this.xIndex = (this.xIndex + 1) % this.cols;
-
-        if (this.xIndex >= this.cols) {
-            this.xIndex = 0;
-        }
-
-        context.drawImage(sheet,
-            this.xIndex * this.spriteWidth, this.yIndex * this.spriteHeight, this.spriteWidth, this.spriteHeight,
-            this.xPos, this.yPos, this.spriteWidth, this.spriteHeight);
-    }
-}*/
-
-/*var xPos = 400;
-var yPos = 300
-var xIndex = 0;
-var yIndex = 1;
-var cols = 6;
-var rows = 4;
-var spriteWidth = 100;
-var spriteHeight = 100;*/
-
-//var hero = new Sprite(400, 300, 0, 1, 6, 4, 100, 100);
-
-
-//var frame = 0;
-
-/*function Animation() {
-    requestAnimationFrame(Animation);
-
-    if ((++frame) % 10 > 0) return;
-    //context.clearRect(0, 0, spriteWidth, spriteHeight);
-    xIndex = (xIndex + 1) % cols;
-
-    if (xIndex >= cols) {
-        xIndex = 0;
-    }
-
-    context.drawImage(sheet,
-        xIndex * spriteWidth, yIndex * spriteHeight, spriteWidth, spriteHeight,
-        0, 0, spriteWidth, spriteHeight);
-}*/
-
 var player = {
     xPos: 0,
     yPos: 200,
@@ -90,7 +23,7 @@ var player = {
     jump: true,
 
     xIndex: 0,
-    yIndex: 1,
+    yIndex: 0,
     cols: 6,
     rows: 4,
     spriteWidth: 100,
@@ -103,18 +36,19 @@ sheet.onload = function () {
 
     Animation();
 }
+
 var frame = 0;
+
 function Animation() {
     requestAnimationFrame(Animation);
-
     if ((++frame) % 10 > 0) return;
-    //context.clearRect(0, 0, spriteWidth, spriteHeight);
     player.xIndex = (player.xIndex + 1) % player.cols;
 
     if (player.xIndex >= player.cols) {
         player.xIndex = 0;
     }
 }
+
 var controller = {
     left: false,
     right: false,
@@ -150,32 +84,23 @@ function MovementLogic() {
 
     if (controller.left) {
         player.yIndex = 3;
-        LoadImg(sheet.src).then(image => {
-            context.drawImage(image,
-                player.xIndex * player.spriteWidth, player.yIndex * player.spriteHeight, player.spriteWidth, player.spriteHeight,
-                player.xPos, player.yPos, player.spriteWidth, player.spriteHeight);
-        });
         player.xVelocity -= 2;
     }
 
     else if (controller.right) {
         player.yIndex = 1;
-        LoadImg(sheet.src).then(image => {
-            context.drawImage(image,
-                player.xIndex * player.spriteWidth, player.yIndex * player.spriteHeight, player.spriteWidth, player.spriteHeight,
-                player.xPos, player.yPos, player.spriteWidth, player.spriteHeight);
-        });
         player.xVelocity += 2;
     }
 
     else {
         player.yIndex = 0;
-        LoadImg(sheet.src).then(image => {
-            context.drawImage(image,
-                player.xIndex * player.spriteWidth, player.yIndex * player.spriteHeight, player.spriteWidth, player.spriteHeight,
-                player.xPos, player.yPos, player.spriteWidth, player.spriteHeight);
-        });
     }
+
+    LoadImg("Pictures/trump.png").then(image => {
+        context.drawImage(image,
+            player.xIndex * player.spriteWidth, player.yIndex * player.spriteHeight, player.spriteWidth, player.spriteHeight,
+            player.xPos, player.yPos, player.spriteWidth, player.spriteHeight);
+    });
 
     if (controller.up && player.jump == false) {
 
