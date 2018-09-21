ee6a034c54ff688f8fb275d33dcf64e4193f1f27
nimp0 <cloud_nimpo@ukr.net>
2018-09-21 17:22:33 +0300

    edited

--- Scripts/Apl.js
+++ Scripts/Apl.js
@@ -3,49 +3,92 @@ var context = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-function LoadImg(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener("load", () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
-}
-
-var player = {
+const DEFAULT_PLAYER_DATA = {
     xPos: 0,
     yPos: 200,
     xVelocity: 0,
-    yVelocity: 1,
+    yVelocity: 0,
     height: 70,
     width: 60,
     jump: true,
 
     xIndex: 0,
-    yIndex: 0,
+    yIndex: 1,
     cols: 6,
     rows: 4,
     spriteWidth: 100,
     spriteHeight: 100,
 };
 
-var sheet = new Image();
-sheet.src = "Pictures/trump.png";
-sheet.onload = function () {
-
-    Animation();
+function CreatePlayerData() {
+    return {...DEFAULT_PLAYER_DATA };
 }
 
-var frame = 0;
+function LoadImg(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener("load", () => {
+            resolve({ image, url });
+        });
+        image.src = url;
+    });
+}
 
-function Animation() {
-    requestAnimationFrame(Animation);
-    if ((++frame) % 10 > 0) return;
-    player.xIndex = (player.xIndex + 1) % player.cols;
+let GLOBAL_BACKGROUND_IMAGE = null;
+let GLOBAL_PLAYER_IMAGE = null;
 
-    if (player.xIndex >= player.cols) {
-        player.xIndex = 0;
+const ASSET_STATE_DATA = {
+    "Pictures/background.png": null,
+    "Pictures/trump.png": null
+};
+
+let currentPlayer = CreatePlayerData();
+console.log(currentPlayer);
+
+ON_ALL_ASSETS_LOADED = () => {
+    GLOBAL_BACKGROUND_IMAGE = ASSET_STATE_DATA["Pictures/background.png"];
+    GLOBAL_PLAYER_IMAGE = ASSET_STATE_DATA["Pictures/trump.png"];
+    requestAnimationFrame(MovementLogic);
+};
+
+function AreAllAssetsLoaded() {
+    let result = true;
+    for (let k in ASSET_STATE_DATA) {
+        if (!ASSET_STATE_DATA[k]) {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+function InitializeAssets() {
+    const resolver = ({ image, url }) => {
+        ASSET_STATE_DATA[url] = image;
+        const isThisAll = AreAllAssetsLoaded();
+        if (isThisAll) {
+            ON_ALL_ASSETS_LOADED();
+        }
+    };
+
+    for (let k in ASSET_STATE_DATA) {
+        LoadImg(k).then(resolver);
+    }
+}
+
+InitializeAssets();
+
+let currentPlayerAnimationFrame = 0;
+function PlayAnimation() {
+    if ((++currentPlayerAnimationFrame) % 10 > 0) {
+        return;
+    }
+    //context.clearRect(0, 0, spriteWidth, spriteHeight);
+    currentPlayer.xIndex = (currentPlayer.xIndex + 1) % currentPlayer.cols;
+
+    if (currentPlayer.xIndex >= currentPlayer.cols) {
+        currentPlayer.xIndex = 0;
     }
 }
 
@@ -71,67 +114,76 @@ var controller = {
     }
 }
 
+function RenderPlayer() {
+    const sx = currentPlayer.xIndex * currentPlayer.spriteWidth;
+    const sy = currentPlayer.yIndex * currentPlayer.spriteHeight;
+    const sw = currentPlayer.spriteWidth;
+    const sh = currentPlayer.spriteHeight;
+
+    const x = currentPlayer.xPos;
+    const y = currentPlayer.yPos;
+
+    const dw = currentPlayer.spriteWidth;
+    const dh = currentPlayer.spriteHeight;
+
+    context.drawImage(GLOBAL_PLAYER_IMAGE, sx, sy, sw, sh, x, y, dw, dh);
+}
+
 function MovementLogic() {
 
-    LoadImg("Pictures/background.png").then(image => {
-        context.drawImage(image, 0, 0, innerWidth, innerHeight);
-    });
-
-    player.xPos += player.xVelocity;
-    player.yPos += player.yVelocity;
-    player.xVelocity *= 0.8;
-    player.yVelocity *= 0.8;
+    currentPlayer.xPos += currentPlayer.xVelocity;
+    currentPlayer.yPos += currentPlayer.yVelocity;
+    currentPlayer.xVelocity *= 0.8;
+    currentPlayer.yVelocity *= 0.8;
 
     if (controller.left) {
-        player.yIndex = 3;
-        player.xVelocity -= 2;
+        currentPlayer.yIndex = 3;
+        currentPlayer.xVelocity -= 2;
     }
 
     else if (controller.right) {
-        player.yIndex = 1;
-        player.xVelocity += 2;
+        currentPlayer.yIndex = 1;
+        currentPlayer.xVelocity += 2;
     }
 
     else {
-        player.yIndex = 0;
+        currentPlayer.yIndex = 0;
     }
 
-    LoadImg("Pictures/trump.png").then(image => {
-        context.drawImage(image,
-            player.xIndex * player.spriteWidth, player.yIndex * player.spriteHeight, player.spriteWidth, player.spriteHeight,
-            player.xPos, player.yPos, player.spriteWidth, player.spriteHeight);
-    });
+    if (controller.up && currentPlayer.jump == false) {
 
-    if (controller.up && player.jump == false) {
-
-        player.yVelocity -= 40;
-        player.jump = true;
+        currentPlayer.yVelocity -= 50;
+        currentPlayer.jump = true;
     }
 
-    if (player.yPos + player.height >= canvas.height - 150) {
-        player.yPos = canvas.height - 125 - player.height;
-        player.jump = false;
+    if (currentPlayer.yPos + currentPlayer.height >= canvas.height - 150) {
+        currentPlayer.yPos = canvas.height - 125 - currentPlayer.height;
+        currentPlayer.jump = false;
     }
 
     else {
-        player.yPos += 20;
+        currentPlayer.yPos += 20;
     }
 
-    if (player.xPos + player.width >= canvas.width) {
-        player.xPos = canvas.width - player.width;
+    if (currentPlayer.xPos + currentPlayer.width >= currentPlayer.width) {
+        currentPlayer.xPos = currentPlayer.width - currentPlayer.width;
     }
 
-    if (player.xPos - player.width <= 0) {
-        player.xPos = 0 + player.width;
+    if (currentPlayer.xPos - currentPlayer.width <= 0) {
+        currentPlayer.xPos = 0 + currentPlayer.width;
     }
+    
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(GLOBAL_BACKGROUND_IMAGE, 0, 0, innerWidth, innerHeight);
+
+    PlayAnimation();
+    RenderPlayer();
 
     requestAnimationFrame(MovementLogic);
 }
 
 addEventListener("keydown", controller.keyListener);
 addEventListener("keyup", controller.keyListener);
-requestAnimationFrame(MovementLogic);
-
 
 
 
